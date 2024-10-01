058eb275ac10589b0432a6259014c4db599772f7
amip888 <amipabdulhamid.888@gmail.com>
2024-10-02 00:20:36 +0700

    update app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Login from "./component/login";
 import Register from "./component/register";
 import Navbar from "./component/navbar";
@@ -6,7 +6,7 @@ import Dashboard from "./component/dashboard";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Login/>}/>
         <Route path="/register" element={<Register/>}/>   
@@ -17,9 +17,34 @@ function App() {
           ]
         }/>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   
   );
 }
 
 export default App;
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import Login from "./component/login";
+// import Register from "./component/register";
+// import Navbar from "./component/navbar";
+// import Dashboard from "./component/dashboard";
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<Login/>}/>
+//         <Route path="/register" element={<Register/>}/>   
+//         <Route path="/dashboard" element={
+//           [
+//             <Navbar/>,
+//             <Dashboard/>
+//           ]
+//         }/>
+//       </Routes>
+//     </BrowserRouter>
+  
+//   );
+// }
+
+// export default App;
