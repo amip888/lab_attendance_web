8a770a39e03ba9e9c09f5a3d0cb6187f084c6153
amip888 <amipabdulhamid.888@gmail.com>
2024-10-01 12:28:04 +0700

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./component/login";
+import Register from "./component/register";
+import Navbar from "./component/navbar";
+import Dashboard from "./component/dashboard";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>   
+        <Route path="/dashboard" element={
+          [
+            <Navbar/>,
+            <Dashboard/>
+          ]
+        }/>
+      </Routes>
+    </BrowserRouter>
+  
+  );
+}
+
+export default App;
